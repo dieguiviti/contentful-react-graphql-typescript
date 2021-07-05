c3d5a5745879bb7dd96dd747ffbd4ddef8441ce6
Diego Gallovich <dieguiviti@gmail.com>
2021-07-05 16:08:13 +0200

    Basic contentful integration with graphql and react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useState, useEffect} from "react";
 import './App.css';
 
+const query = `
+{
+  pageCollection {
+    items {
+      title
+      logo {
+        url
+      }
+    }
+  }
+}
+`
+
+type Page = {
+  title: string;
+  logo: {
+    url: string
+  }
+}
+
+const contentfulSpaceId = process.env.REACT_APP_CONTENTFUL_SPACE_ID
+const contentfulAccessToken = process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN
+
 function App() {
-  return (
+
+  const [page, setPage] = useState({} as Page)
+
+  useEffect(() => {
+    window.fetch(
+      `https://graphql.contentful.com/content/v1/spaces/${contentfulSpaceId}/`,
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${contentfulAccessToken}`
+        },
+        body: JSON.stringify({ query })
+      }
+    ).then(res => {
+      return res.json()
+    }).then(({ data, errors }) => {
+      if (errors) {
+        console.log(errors)
+      }
+      console.log(data)
+
+      setPage(data.pageCollection.items[0])
+    })
+  }, [])
+
+
+  return page ? (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={page.logo ? page.logo.url : ''} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {page ? page.title : ''}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
-  );
+  ) : <h1>Loading...</h1>;
 }
 
 export default App;
